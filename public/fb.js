99da76a0f843c684f98411656e663afbd8badcef
fengzibin <8688256@qq.com>
2017-01-15 20:29:46 +0800

    demo fb

--- public/fb.js
+++ public/fb.js
@@ -1,12 +1,9 @@
-(function(){
+(window.addEventListener("load",function(){
     var that = this;
     this.summary="";
     this.description="";
-    this.init()=function(){
         var box = document.createElement('div');
-        box.style.cssText="position:fixed;width:200px;height:200px;border:1px solid #999;background:#fff;left:0px;top:30px;";
-        box.innerHTML="<span>内容</span><span>内容</span><span>内容</span>";
+        box.style.cssText="position:fixed;width:20px;height:40px;border:1px solid #999;background:#eeeeee;right:5px;bottom:100px;color:#333333";
+        box.innerHTML="<span>反馈</span>";
         document.body.appendChild(box);
-    };
-
-})();
+}));
